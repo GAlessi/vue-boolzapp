ea66bbd21581d43587b1eff465572a9a1adf9c92
Gabriele Alessi <89archaon89@gmail.com>
2021-04-11 17:30:40 +0200

    Tentativo di apertura e chiusura menù dropdown

--- js/script.js
+++ js/script.js
@@ -18,121 +18,125 @@ function init() {
                             date: '10/01/2020',
                             time: '15:30:55',
                             text: 'Ehi V, hai visto la mia nuova braindance?',
-                            status: 'received'
+                            status: 'received',
+                            menu: false
                         },
                         {
                             date: '10/01/2020',
                             time: '15:50:00',
                             text: 'Ricordati di mangiare oggi',
-                            status: 'received'
+                            status: 'received',
+                            menu: false
                         },
                         {
                             date: '10/01/2020',
                             time: '16:15:22',
                             text: 'Già fatto, è un capolavoro!',
-                            status: 'sent'
+                            status: 'sent',
+                            menu: false
                         },
                         {
                             date: '10/01/2020',
                             time: '16:15:22',
                             text: 'come sempre...',
-                            status: 'sent'
+                            status: 'sent',
+                            menu: false
                         }
                     ],
-                },
-                {
-                    name: 'Jackie',
-                    avatar: 'jackie',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '20/03/2020',
-                            time: '16:30:00',
-                            text: 'Ciao choom, come stai?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '20/03/2020',
-                            time: '16:30:55',
-                            text: "Bene grazie! Stasera ci vediamo all'Alterlife?",
-                            status: 'received'
-                        },
-                        {
-                            date: '20/03/2020',
-                            time: '16:35:00',
-                            text: 'Mi piacerebbe ma devo vedere Judy',
-                            status: 'sent'
-                        }
-                    ],
-                },
-                {
-                    name: 'Wakako',
-                    avatar: 'wakako',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '28/03/2020',
-                            time: '10:10:40',
-                            text: 'Devo fare sparire il mio ottavo marito',
-                            status: 'received'
-                        },
-                        {
-                            date: '28/03/2020',
-                            time: '10:20:10',
-                            text: 'Sicura di non aver sbagliato chat?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '28/03/2020',
-                            time: '16:15:22',
-                            text: 'Ah',
-                            status: 'received'
-                        },
-                        {
-                            date: '28/03/2020',
-                            time: '16:15:22',
-                            text: 'dimentica tutto, V',
-                            status: 'received'
-                        },
-                        {
-                            date: '28/03/2020',
-                            time: '16:15:22',
-                            text: 'sul serio V, dimentica tutto',
-                            status: 'received'
-                        }
-                    ],
-                },
-                {
-                    name: 'Panam',
-                    avatar: 'panam',
-                    visible: true,
-                    messages: [
-                        {
-                            date: '10/01/2020',
-                            time: '15:30:55',
-                            text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020',
-                            time: '15:50:00',
-                            text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
-                        }
-                    ],
-                },
+                }
+            //     {
+            //         name: 'Jackie',
+            //         avatar: 'jackie',
+            //         visible: true,
+            //         messages: [
+            //             {
+            //                 date: '20/03/2020',
+            //                 time: '16:30:00',
+            //                 text: 'Ciao choom, come stai?',
+            //                 status: 'sent'
+            //             },
+            //             {
+            //                 date: '20/03/2020',
+            //                 time: '16:30:55',
+            //                 text: "Bene grazie! Stasera ci vediamo all'Alterlife?",
+            //                 status: 'received'
+            //             },
+            //             {
+            //                 date: '20/03/2020',
+            //                 time: '16:35:00',
+            //                 text: 'Mi piacerebbe ma devo vedere Judy',
+            //                 status: 'sent'
+            //             }
+            //         ],
+            //     },
+            //     {
+            //         name: 'Wakako',
+            //         avatar: 'wakako',
+            //         visible: true,
+            //         messages: [
+            //             {
+            //                 date: '28/03/2020',
+            //                 time: '10:10:40',
+            //                 text: 'Devo fare sparire il mio ottavo marito',
+            //                 status: 'received'
+            //             },
+            //             {
+            //                 date: '28/03/2020',
+            //                 time: '10:20:10',
+            //                 text: 'Sicura di non aver sbagliato chat?',
+            //                 status: 'sent'
+            //             },
+            //             {
+            //                 date: '28/03/2020',
+            //                 time: '16:15:22',
+            //                 text: 'Ah',
+            //                 status: 'received'
+            //             },
+            //             {
+            //                 date: '28/03/2020',
+            //                 time: '16:15:22',
+            //                 text: 'dimentica tutto, V',
+            //                 status: 'received'
+            //             },
+            //             {
+            //                 date: '28/03/2020',
+            //                 time: '16:15:22',
+            //                 text: 'sul serio V, dimentica tutto',
+            //                 status: 'received'
+            //             }
+            //         ]
+            //     },
+            //     {
+            //         name: 'Panam',
+            //         avatar: 'panam',
+            //         visible: true,
+            //         messages: [
+            //             {
+            //                 date: '10/01/2020',
+            //                 time: '15:30:55',
+            //                 text: 'Lo sai che ha aperto una nuova pizzeria?',
+            //                 status: 'sent'
+            //             },
+            //             {
+            //                 date: '10/01/2020',
+            //                 time: '15:50:00',
+            //                 text: 'Si, ma preferirei andare al cinema',
+            //                 status: 'received'
+            //             }
+            //         ],
+            //     },
             ]
         },
 
         methods:{
 
-            // Dichiara di quale contatto mostrare la chat e aggiungo il focus sul'imput nuovo messaggi0
+            // Dichiara di quale contatto mostrare la chat e aggiunge il focus sul'imput nuovo messaggio
             getContact: function (index) {
                 this.activeContact=this.contactsArray[index];
                 this.$nextTick(() => this.$refs.newMsg.focus());
             },
 
-            //In caso un numero sia minore di 10 aggiunge un 0 davati per mantenere il formato a 2 cifre
+            //In caso un numero sia minore di 10 aggiunge uno 0 davanti per mantenere il formato a 2 cifre
             plusZero: function(timeNum) {
               if (timeNum  < 10) {
                   timeNum = '0' + timeNum;
@@ -171,6 +175,11 @@ function init() {
                });
            },
 
+           // mostra il menù dei messaggi
+           showMenu: function (message) {
+
+                message['menu']= !message['menu'];
+           }
         }
     });
 }
