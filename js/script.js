6cd700ef214264739d7bf111a5c45741a3d2ab5c
Gabriele Alessi <89archaon89@gmail.com>
2021-04-13 16:28:12 +0200

    Eliminata possibilità di aprire dropdown di due msg contemporaneamente

--- js/script.js
+++ js/script.js
@@ -20,32 +20,24 @@ function init() {
                             time: '15:30:55',
                             text: 'Ehi V, hai visto la mia nuova braindance?',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '10/01/2020',
                             time: '15:50:00',
                             text: 'Ricordati di mangiare oggi',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '10/01/2020',
                             time: '16:15:22',
                             text: 'Già fatto, è un capolavoro!',
                             status: 'sent',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '10/01/2020',
                             time: '16:15:22',
                             text: 'come sempre...',
                             status: 'sent',
-                            menu: false,
-                            chevron: false
                         }
                     ],
                 },
@@ -59,24 +51,18 @@ function init() {
                             time: '16:30:00',
                             text: 'Ciao choom, come stai?',
                             status: 'sent',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '20/03/2020',
                             time: '16:30:55',
                             text: "Bene grazie! Stasera ci vediamo all'Alterlife?",
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '20/03/2020',
                             time: '16:35:00',
                             text: 'Mi piacerebbe ma devo vedere Judy',
                             status: 'sent',
-                            menu: false,
-                            chevron: false
                         }
                     ],
                 },
@@ -90,40 +76,30 @@ function init() {
                             time: '10:10:40',
                             text: 'Devo fare sparire il mio ottavo marito',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '28/03/2020',
                             time: '10:20:10',
                             text: 'Sicura di non aver sbagliato chat?',
                             status: 'sent',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '28/03/2020',
                             time: '16:15:22',
                             text: 'Ah',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '28/03/2020',
                             time: '16:15:22',
                             text: 'dimentica tutto, V',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         },
                         {
                             date: '28/03/2020',
                             time: '16:15:22',
                             text: 'sul serio V, dimentica tutto',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         }
                     ]
                 },
@@ -137,8 +113,6 @@ function init() {
                             time: '15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent',
-                            menu: false,
-                            chevron: false
 
                         },
                         {
@@ -146,27 +120,29 @@ function init() {
                             time: '15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
                             status: 'received',
-                            menu: false,
-                            chevron: false
                         }
                     ],
                 },
             ]
         },
         updated() {
-            const container = this.$el.querySelector("#chat");
-            container.scrollTop = container.scrollHeight;
+            if (this.activeContact) {
+                const container = this.$el.querySelector("#chat");
+                container.scrollTop = container.scrollHeight;
+            }
         },
-        // mounted: function () {
-        //             for (let i = 0; i < this.contactsArray.length; i++) {
-        //                 const contact=this.contactsArray[i];
-        //                 for (let x = 0; x < contact.messages.length; x++) {
-        //                     const message=contact.messages[x];
-        //                     message['menu'] = false;
-        //                 }
-        //             };
-        //             console.log(this.contactsArray);
-        // },
+
+        mounted: function () {
+                    for (let i = 0; i < this.contactsArray.length; i++) {
+                        const contact=this.contactsArray[i];
+                        for (let x = 0; x < contact.messages.length; x++) {
+                            const message=contact.messages[x];
+                            Vue.set(message, 'menu', false);
+                            Vue.set(message, 'chevron', false);
+                        }
+                    };
+                    console.log(this.contactsArray);
+        },
         methods:{
 
             // Dichiara di quale contatto mostrare la chat e aggiunge il focus sul'imput nuovo messaggio
@@ -219,10 +195,18 @@ function init() {
 
            // mostra/nasconde il menù dei messaggi con click su chevron
            toggleMenu: function (message) {
-               message.menu = !message.menu;
-               console.log(message.menu);
-           },
+               let msg="";
+               for (let i = 0; i < this.activeContact.messages.length; i++) {
+                    msg = this.activeContact.messages[i].menu;
+                       msg=false
+               };
 
+               if (message.menu && !msg) {
+                   message.menu = false;
+               }else {
+                   message.menu=true;
+               }
+           },
 
            //chiude il dropdown con click esterno
            closeMenu: function () {
@@ -239,10 +223,14 @@ function init() {
             },
 
             //mostra lo chevron a hover sul messaggio
-            toggleChevron:function (message) {
-                message.chevron = !message.chevron;
+            showChevron:function (message) {
+                message.chevron = true;
+            },
 
-            }
+            //nasconde lo chevron e mostra la data
+            hideChevron:function (message) {
+                message.chevron = false;
+            },
         }
     });
 }
